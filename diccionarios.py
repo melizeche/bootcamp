f8aa445eb28208b127d69c20cd362908cc0c53e4
Marcelo Elizeche Landó <melizeche@gmail.com>
2019-07-17 15:55:38 -0400

    mejor explicado lo de diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -1,3 +1,22 @@
+#Diccionario de datos
+
+# Un diccionario es una estructura de datos y un tipo de dato en Python
+# que nos permite almacenar pares de clave:valor
+# Los diccionarios nos permiten identicar los elementos por una 
+# clave(key)
+
+nombre_del_diccionario = { "nombre_de_la_clave":"valor de cualquier tipo"}
+diccionario_vacio = {}
+
+dic_persona = {"nombre":"Marce", "edad":33 }
+
+#Para acceder a un valor ponemos el nombre del diccionario y entre[]
+#la clave
+print(dic_persona["nombre"])
+# Para cambiar el valor ponemos el nombre del dic, entre[] la clave 
+# y asignamos con =
+dic_persona["edad"] = 99
+
 nombre = "Steven"
 lista_hobbies = ["rugby","comer mbeju","cortar patas de dinos"]
 idiomas = {
@@ -6,7 +25,6 @@ idiomas = {
     "guarani":"jopara", 
     "portugues":"kaure"
     }
-
 persona = {
     "nombre": nombre, 
     "edad":33, 
